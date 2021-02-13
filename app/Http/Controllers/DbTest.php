cc14895cc22bd5df05cdfadb439b123951faf754
Epiloum J. Kim <epiloum@gmail.com>
2021-02-13 22:13:40 +0900

    Add the first controller

---
+++ app/Http/Controllers/DbTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DbTest extends Controller
+{
+    public function test()
+    {
+        return 'TEST 123';
+    }
+}
