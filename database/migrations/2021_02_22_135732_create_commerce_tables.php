97eece48dbe8d2848e9452a27dbe91dbaff9ff49
Epiloum J. Kim <epiloum@gmail.com>
2021-02-22 23:13:03 +0900

    Add the first migration code

---
+++ database/migrations/2021_02_22_135732_create_commerce_tables.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommerceTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id('no');
+            $table->string('email', 256)->unique();
+            $table->string('password', 64);
+            $table->tinyInteger('grade');
+            $table->timestamps();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('no');
+            $table->unsignedBigInteger('user_register');
+            $table->string('title', 255);
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('inventory');
+            $table->timestamps();
+
+            $table->foreign('user_register')->references('no')->on('users');
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('no');
+            $table->unsignedBigInteger('user_buy');
+            $table->unsignedBigInteger('user_sell');
+            $table->unsignedBigInteger('product_no');
+            $table->unsignedInteger('qty');
+            $table->unsignedInteger('amt');
+            $table->text('requirements');
+            $table->timestamps();
+
+            $table->foreign('user_buy')->references('no')->on('users');
+            $table->foreign('user_sell')->references('no')->on('users');
+            $table->foreign('product_no')->references('no')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('orders');
+    }
+}
