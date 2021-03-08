c3db265385f5620aece938555e20286bf04d87db
Epiloum J. Kim <epiloum@gmail.com>
2021-03-08 22:10:06 +0900

    Change the name of DbTest controller

--- routes/web.php
+++ routes/web.php
@@ -38,61 +38,8 @@ Route::get('/test/mw', function () {
 })->middleware(App\Http\Middleware\MwTest::class);
 
 // Controller & DB Query Test
-Route::get('/test/db', [App\Http\Controllers\DbTest::class, 'test_query']);
-
-Route::get('/test/db/collection', function () {
-    $res = [];
-
-    // contains()
-    $orders = App\Models\Order::all();
-    $res['contains1'] = $orders->contains('user_buy', '=', '160');
-    $res['contains2'] = $orders->contains('user_buy', '=', '1000');
-
-    // intersect() and diff()
-    $res['intersect'] = $orders->intersect(App\Models\Order::where('amt', '>=', 100000)->get());
-    $res['diff'] = $orders->diff(App\Models\Order::where('amt', '<', 100000)->get());
-
-    // only() and except()
-    $res['only'] = $orders->only([1,3,5]);
-    $res['except'] = $orders->except([1,3,5,7,9,11,13,15]);
-
-    // find()
-    $res['find'] = $orders->find([1,3,5]);
-
-    // modelkeys()
-    $res['modelkeys'] = $orders->modelkeys();
-
-    // makeVisible() and makeHidden() / It doesn't work clone! Is it Singleton?!
-    $tmp1 = App\Models\Order::all();
-    $tmp2 = App\Models\Order::all();
-    $res['makeHidden'] = $tmp1->makeHidden(['user_buy', 'product_id', 'requirements', 'created_at', 'updated_at']);
-    $res['makeVisible'] = $tmp2->makeHidden(['user_buy', 'product_id', 'requirements', 'created_at', 'updated_at'])->makeVisible(['product_id']);
-
-    // unique()
-    $res['unique'] = $orders->unique('product_id');
-
-    // fresh(), load(), loadMissing(), toQuery()
-
-    // Export
-    ob_start();
-    echo '<table>';
-    foreach ($res as $k => $v) {
-        echo '<tr>';
-        echo '<th>' . $k . '</th><td>';
-        if(is_iterable($v)) {
-            foreach($v as $v2) {
-                echo $v2 . '<br />';
-            }
-        } else {
-            var_dump($v);
-        }
-        echo '</td>';
-        echo '</tr>';
-    }
-    echo '</table>';
-
-    return ob_get_clean();
-});
+Route::get('/test/db', [App\Http\Controllers\DbTestController::class, 'test_query']);
+Route::get('/test/db/collection', [App\Http\Controllers\DbTestController::class, 'test_collection']);
 
 // Sign in for Validation Test
 Route::get('/signin', [App\Http\Controllers\SignIn::class, 'frm']);
