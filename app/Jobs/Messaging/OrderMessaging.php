afcdd1f3ad067920679d3da902630bd7f681666e
Epiloum J. Kim <epiloum@gmail.com>
2021-03-28 00:21:00 +0900

    Add the first queue for testing

---
+++ app/Jobs/Messaging/OrderMessaging.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs\Messaging;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use App\Models\Order;
+
+class OrderMessaging implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $orderId;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param string $orderId
+     * @return void
+     */
+    public function __construct(string $orderId)
+    {
+        $this->orderId = $orderId;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::channel('slack')->info("여기에 알림입력\n(입력값 : " . $this->orderId . ")");
+    }
+}
